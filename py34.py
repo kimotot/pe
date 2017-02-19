be78f95b62db27f5d2684e546d9a9d56f612c154
Takeshi Kimoto <god4bid@hear.to>
2017-02-19 11:30:36 +0900

    mac上でPyCharmを使用して改めて修正した

--- py34.py
+++ py34.py
@@ -2,9 +2,13 @@
 
 
 def kai(n):
-    if n == 1:
-        return 1
-    else:
-        return n * kai(n - 1)
+	"""
+	正の整数の階乗を求める関数
+	:rtype: 階乗の計算結果
+	"""
+	if n == 1:
+		return 1
+	else:
+		return n * kai(n - 1)
 
-print(kai(10))
+print(kai(11))
