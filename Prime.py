f1575da84499e154f188e563150f82bc41965222
Takeshi Kimoto <god4bid@hear.to>
2017-03-04 15:00:13 +0900

    順列を求める関数とイテレータを作成

---
+++ Prime.py
@@ -0,0 +1,115 @@
+# 素数を列挙するイテレータクラスのモジュールファイル
+
+import math
+
+class PrimeIter:
+    '''素数を列挙するイテレータクラス'''
+
+    def __init__(self, max = 0):
+        ''' 生成する素数の最大値を設定する
+            値の最小値は２
+            引数が２以下の数字であったら、maxに０を設定する。この場合、無限に素数を生成する
+            1は素数ではない'''
+        if max < 2:
+            self._max = 0
+        else:
+            self._max = max
+
+    def __iter__(self):
+        '''イテレータとして初期化する'''
+        self._n = 1          # 1の次の数字（２）から素数の探索を始める
+        self._prime_list = []    # クラス内部で求めた素数を保持するリスト
+        return self
+
+    def __next__(self):
+        '''nの次の素数を探索して返す'''
+        while True:
+            self._n += 1
+            if self._max !=0 and self._n > self._max:
+                raise StopIteration
+
+            # 今までに見つけた全ての素数で割り切れなければ素数である
+            # もしくは割ろうと試みる数字が、sqrt(n)以上になれば素数である
+            is_prime = True
+            q = math.sqrt(self._n)
+
+            for p in self._prime_list:
+                if p > q:
+                    is_prime = True
+                    break
+
+                if self._n % p == 0:
+                    is_prime = False
+                    break
+
+            if is_prime:
+                self._prime_list.append(self._n)
+                return self._n
+
+    def get_prime_list(self):
+        '''インスタンス内部で保持している素数リストを返す関数'''
+        return self._prime_list
+
+    def get_nth_prime(self,nth):
+        ''' ２から初めてn番目の素数を返す関数
+            先頭の素数は０番目とする'''
+        if nth <= len(self._prime_list):
+            return self._prime_list[nth]
+        else:
+            raise IndexError
+
+
+class Prime:
+            '''素数をリスト形式で保持するクラス'''
+
+            def __init__(self, max = 2):
+                ''' 生成する素数の最大値を設定する
+                    値の最小値は２
+                    引数が２以下の数字であったら、maxに強制的に２を設定する。
+                    1は素数ではない'''
+
+                self._prime_list = []
+
+                if max < 2:
+                    self._max = 2
+                else:
+                    self._max = max
+
+                self._n = 2
+                while self._n <= self._max:
+                    # 今までに見つけた全ての素数で割り切れなければ素数である
+                    # もしくは割ろうと試みる数字が、sqrt(n)以上になれば素数である
+                    is_prime = True
+                    q = math.sqrt(self._n)
+
+                    for p in self._prime_list:
+                        if p > q:
+                            is_prime = True
+                            break
+
+                        if self._n % p == 0:
+                            is_prime = False
+                            break
+
+                    if is_prime:
+                        self._prime_list.append(self._n)
+
+                    self._n += 1
+
+            def get_prime_list(self):
+                '''インスタンス内部で保持している素数リストを返す関数'''
+                return self._prime_list
+
+            def get_nth_prime(self, nth):
+                ''' ２から初めてn番目の素数を返す関数
+                    先頭の素数は０番目とする'''
+                if nth <= len(self._prime_list):
+                    return self._prime_list[nth]
+                else:
+                    raise IndexError
+
+
+# 100以下の素数を列挙する
+if __name__ == "__main__":
+    p = Prime(-1)
+    print(p.get_prime_list())
--- py32.py
+++ py32.py
@@ -1,11 +1,55 @@
 # coding:UTF-8
 
+import copy
+import time
+
+def permutations(origin):
+    ''' 与えられたリスト要素の順列を求める関数
+        引数はリストなど、イテーラブルなもの
+        戻値は全ての順列を要素としてリストにしたもの
+        再帰呼び出し関数'''
+
+    if len(origin) == 0:
+        return [[]]
+    else:
+        ans = []
+        for index,header in enumerate(origin):
+            new_orirign = copy.deepcopy(origin)
+            del new_orirign[index]
+
+            for cuder in permutations(new_orirign):
+                cuder.insert(0,header)
+                ans.append(copy.deepcopy(cuder))
+
+        return ans
+
+
+
+def permutationsIt(origin):
+    ''' 与えられたリスト要素の順列を求める関数
+        引数はリストなど、イテーラブルなもの
+        戻値は全ての順列を要素としてリストにしたもの
+        再帰呼び出し関数'''
+
+    if len(origin) == 0:
+        yield []
+    else:
+        for index, header in enumerate(origin):
+            new_orign = copy.deepcopy(origin)
+            del new_orign[index]
+
+            for cuder in permutationsIt(new_orign):
+                cuder.insert(0, header)
+                yield cuder
+
+
+
 # 整数値のリスト
 li = [1,2,3,4,5,6,7,8,9]
 
 def pickone(selected):
     if len(li) == 0:
-        print(selected)
+        yield selected
     else:
         for ix,x in enumerate(li):
             del li[ix]
@@ -15,8 +59,15 @@ def pickone(selected):
             selected.pop()
             li.insert(ix,x)
 
-def pan23(selected):
-    selected
 
-pickone([])
+if __name__ == "__main__":
+
+    start = time.time()
+
+    for n in permutationsIt([1,2,3,4,5,6,7,8,9]):
+        pass
+
+    elapsed_time = time.time() - start
+    print("処理時間={0:.4f}".format(elapsed_time))
+
 
